8ac1b8cd142ea1cdf7dc274e9b4fa00463c5a93c
Udemy - Tien <hien.khuu.learn@gmail.com>
2024-12-03 10:14:51 +0700

    feat: update javascript ex3

--- tests/lesson-02/ex3.js
+++ tests/lesson-02/ex3.js
@@ -1,9 +1,29 @@
-const myNumber = -60;
+const myNumber = "a";
+let condition;
 
 if (myNumber > 0) {
     console.log("Giá trị bạn nhập là số dương");
+    condition = "positive";
 } else if (myNumber < 0) {
     console.log("Giá trị bạn nhập là số âm");
-} else {
+    condition = "negative";
+} else if (myNumber === 0) {
     console.log("Giá trị bạn nhập là số 0");
+    condition = "zero";
+} else {
+    console.log("Unknow inputted");
+}
+
+switch (condition) {
+    case "positive":
+        console.log("Switch Case - Giá trị bạn nhập là số dương");
+        break;
+    case "negative":
+        console.log("Switch Case - Giá trị bạn nhập là số âm");
+        break;
+    case "zero":
+        console.log("Switch Case - Giá trị bạn nhập là số 0");
+        break;
+    default:
+        console.log("Switch Case - Unknow inputted");
 }
