5a6b728ae964a4d2961c91293fb489673fa46382
Udemy - Tien <hien.khuu.learn@gmail.com>
2024-12-03 10:21:47 +0700

    chore: update typo ex4

--- tests/lesson-02/ex4.js
+++ tests/lesson-02/ex4.js
@@ -1,6 +1,6 @@
 const myHeight = 170;
-const googWeight = (myHeight % 100) * 9 / 10;
+const goodWeight = (myHeight % 100) * 9 / 10;
 const maxWeight = myHeight % 100;
 const minWeight = (myHeight % 100) * 8 / 10;
 
-console.log("cân nặng lý tưởng: " + googWeight + "kg, cân nặng tối đa: " + maxWeight + "kg, cân nặng tối thiểu: " + minWeight + "kg");
+console.log("cân nặng lý tưởng: " + goodWeight + "kg, cân nặng tối đa: " + maxWeight + "kg, cân nặng tối thiểu: " + minWeight + "kg");
