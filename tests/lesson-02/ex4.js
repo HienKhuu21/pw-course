e243415cd2048c9032862c752fcb2fa1bc21587b
Udemy - Tien <hien.khuu.learn@gmail.com>
2024-12-04 21:21:06 +0700

    fix: add variable for modheight to reuse

--- tests/lesson-02/ex4.js
+++ tests/lesson-02/ex4.js
@@ -1,6 +1,7 @@
 const myHeight = 170;
-const goodWeight = (myHeight % 100) * 9 / 10;
+const height = myHeight % 100;
+const goodWeight = height * 9 / 10;
 const maxWeight = myHeight % 100;
-const minWeight = (myHeight % 100) * 8 / 10;
+const minWeight = height * 8 / 10;
 
 console.log("cân nặng lý tưởng: " + goodWeight + "kg, cân nặng tối đa: " + maxWeight + "kg, cân nặng tối thiểu: " + minWeight + "kg");
