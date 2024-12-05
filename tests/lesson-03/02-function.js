b7eb03816c97e90b5de505122c1b27620fc3e1d4
Udemy - Tien <hien.khuu.learn@gmail.com>
2024-12-05 22:04:36 +0700

    feat: add ex4 of function exercise

--- tests/lesson-03/02-function.js
+++ tests/lesson-03/02-function.js
@@ -1,4 +1,4 @@
-// -------------------------------- Exercise 1
+// ----------------------------- --- Exercise 1
 
 function bmiCalculate(heightByMetter, weightByKilo) {
     let bmi = weightByKilo / (heightByMetter * heightByMetter);
@@ -20,13 +20,13 @@ let student = {
     student4: { h: 1.5, w: 100 }
 }
 
-console.log("Exercise 1 result:")
+console.log("\n --- Exercise 1 result --- \n");
 for (let i in student) {
     let bmiResult = bmiCalculate(student[i].h, student[i].w);
     console.log(bmiResult);
 }
 
-// -------------------------------- Exercise 2
+// ----------------------------- --- Exercise 2
 
 function convertTemperature(temperature, type) {
     if (type === "C") {
@@ -38,9 +38,72 @@ function convertTemperature(temperature, type) {
     }
 }
 
-console.log("Exercise 2 result:")
+console.log("\n --- Exercise 2 result --- \n");
 console.log(convertTemperature(28, "C"));
 console.log(convertTemperature(82.4, "F"));
 
+// ----------------------------- --- Exercise 3
 
-// -------------------------------- Exercise 3
+let totalNum = 5;
+let myArray = [];
+
+for (let i = 1; i <= totalNum; i++) {
+    myArray.push(Math.floor(Math.random() * 10));
+}
+
+console.log("\n --- Exercise 3 result --- \n");
+console.log("myArray value is: ", myArray);
+
+function sumArrayItem(array) {
+    let sumArray = 0;
+    for (let i = 0; i < array.length; i++) {
+        sumArray = sumArray + array[i];
+    }
+    return sumArray;
+}
+
+console.log("Sum of myArray = ", sumArrayItem(myArray));
+
+// ----------------------------- --- Exercise 4
+
+// Init Prime Array
+let totalPrimeNum = 10;
+let myPrimeArray = [];
+
+for (let i = 1; i <= totalPrimeNum; i++) {
+   myPrimeArray.push(Math.floor(Math.random() * 100));
+}
+console.log("\n --- Exercise 4 Result --- \n");
+console.log("myArray value is: ", myPrimeArray);
+
+function findPrimeNumber(array) {
+    let primeNumArray = []; 
+    let notPrimeNumArray = [];
+    for (let i = 0; i < array.length; i++) {
+        //console.log("\n checking " + array[i] + " is Prime Number or not.");
+        let isPrimeNum = true;
+        if (array[i] < 2) {
+            notPrimeNumArray.push(array[i]);
+        } else {
+            for (let j = 2; j <= array[i] - 1; j++) {
+                if (array[i] % j === 0) {
+                    isPrimeNum = false;
+                    notPrimeNumArray.push(array[i]);
+                    break;
+                }
+            }
+            if (isPrimeNum == true) {
+                primeNumArray.push(array[i]);
+            }
+        }
+    }
+    // Print notPrimeNumArray for testing
+    // console.log("notPrimeNumArray: ", notPrimeNumArray);    
+    // Return value
+    if (primeNumArray.length > 0) {
+        return primeNumArray;
+    } else {
+        return "Not found Prime Number from inputed array";
+    }
+}
+console.log(findPrimeNumber(myPrimeArray));
