ac50699f4e370fedc55c5ca038129d1de2f1bbaf
Udemy - Tien <hien.khuu.learn@gmail.com>
2024-12-18 09:37:07 +0700

    chore: add test step description, remove unused line

--- tests/lesson-05/test1.spec.ts
+++ tests/lesson-05/test1.spec.ts
@@ -8,7 +8,7 @@ function convertDateString(dateString: string): string {
     return `${year}-${month}-${day}`;
 }
 
-test('Register', async ({ page }) => {
+test('Register Page', async ({ page }) => {
 
     let userName = "Tien";
     let userEmail = "hien.khuu.learn@gmail.com";
@@ -22,57 +22,73 @@ test('Register', async ({ page }) => {
     let rating = 8;
     let color = "#0400ff";
 
-    await page.goto('https://material.playwrightvn.com/');
+    await test.step('Step 1: Truy cập trang https://material.playwrightvn.com/', async () => {
+        await page.goto('https://material.playwrightvn.com/');
+    })
 
-    await page.locator('//a[contains(text(),"Bài học 1: Register Page")]').click();
+    await test.step('Step 2: click vào "Bài học 1: Register Page" link', async () => {
+        await page.locator('//a[contains(text(),"Bài học 1: Register Page")]').click();
+    })
 
-    await page.locator('//input[@id="username"]').fill(userName);
+    await test.step('Step 3: Nhập đầy đủ các thông tin, click button register', async () => {
+        // Input Username
+        await page.locator('//input[@id="username"]').fill(userName);
 
-    await page.locator('//input[@id="email"]').fill(userEmail);
+        // Input Email
+        await page.locator('//input[@id="email"]').fill(userEmail);
 
-    await page.locator('//input[@id="' + gender + '"]').setChecked(true);
-
-    /*if (gender==="male"){
-        await page.locator('//input[@id="' + gender + '"]').check();
-    } else {
+        // Select Gender
         await page.locator('//input[@id="' + gender + '"]').setChecked(true);
-    }*/
 
-    //await page.locator('//input[@id="reading"]').check();
+        /*if (gender==="male"){
+            await page.locator('//input[@id="' + gender + '"]').check();
+        } else {
+            await page.locator('//input[@id="' + gender + '"]').setChecked(true);
+        }*/
 
-    for (let i in hobbies) {
-        let hoopy = hobbies[i].toLowerCase();
-        //let xpath = "'//input[@id=\"" + hoopy + "\"]'";
-        //console.log(xpath);
-        await page.locator('//input[@id="' + hoopy + '"]').check();
-    }
+        // Select Hoppies
+        for (let i in hobbies) {
+            let hoopy = hobbies[i].toLowerCase();
+            await page.locator('//input[@id="' + hoopy + '"]').check();
+        }
 
-    await page.locator('//select[@id="interests"]').selectOption(interests);
+        // Select Interests
+        await page.locator('//select[@id="interests"]').selectOption(interests);
 
-    /*for (let i in interests) {
-        await page.locator('//select[@id="interests"]//option[text()="' + interests[i] + '"]').click({
-            modifiers: ['Control'],
-            });
-    }*/
+        /*for (let i in interests) {
+            await page.locator('//select[@id="interests"]//option[text()="' + interests[i] + '"]').click({
+                modifiers: ['Control'],
+                });
+        }*/
 
-    await page.locator('//select[@id="country"]').selectOption(countries['United Kingdom']);
-    //await page.locator('//select[@id="country"]').selectOption({ value: countries['Canada'] });
+        // Select Country
+        await page.locator('//select[@id="country"]').selectOption(countries['United Kingdom']);
+        //await page.locator('//select[@id="country"]').selectOption({ value: countries['Canada'] });
 
-    //await page.locator('//input[@id="dob"]').fill("1985-01-01");
-    await page.locator('//input[@id="dob"]').fill(convertDateString(dob));
+        // Input Date of Birth
+        await page.locator('//input[@id="dob"]').fill(convertDateString(dob));
 
-    await page.locator('//input[@id="profile"]').setInputFiles(personalImage);
+        // Select Image
+        await page.locator('//input[@id="profile"]').setInputFiles(personalImage);
 
-    await page.locator('//textarea[@id="bio"]').fill(bio);
+        // Input Bio
+        await page.locator('//textarea[@id="bio"]').fill(bio);
 
-    await page.locator('//input[@id="rating"]').fill(rating.toString());
+        // Select Rating
+        await page.locator('//input[@id="rating"]').fill(rating.toString());
 
-    await page.locator('//input[@id="favcolor"]').fill(color);
+        // Select Color
+        await page.locator('//input[@id="favcolor"]').fill(color);
 
-    await page.locator('//input[@id="newsletter"]').check();
+        // Select Newsletter
+        await page.locator('//input[@id="newsletter"]').check();
 
-    await page.locator('//span[@class="slider round"]').click();
+        // Select Enable Feature
+        await page.locator('//span[@class="slider round"]').click();
 
-    await page.locator('//button[text()="Register"]').click();
+        // Click Register button
+        await page.locator('//button[text()="Register"]').click();
+    })
+
     //await page.close();
 });
